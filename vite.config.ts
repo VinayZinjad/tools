272b5651452feceb3b7b3e98d8c453e78c14d391
Vinay Zinjad <vinay.zinjad@theinfozest.com>
2024-02-13 00:44:29 +0530

    add: pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,42 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+	registerType: "prompt",
+	includeAssets: ["logo.png", "logo.svg"],
+	manifest: {
+		name: "Tools",
+		short_name: "Tools",
+		description: "An app to create branch name from jira.",
+		icons: [
+			{
+				src: "/logo.png",
+				sizes: "512x512",
+				type: "image/png",
+				purpose: "any maskable",
+			},
+		],
+		theme_color: "#171717",
+		background_color: "#e8ebf2",
+		display: "standalone",
+		scope: "/",
+		start_url: "/",
+		orientation: "portrait",
+	},
+};
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA(manifestForPlugin),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base:"tools"
-})
+  base: "/tools",
+});
